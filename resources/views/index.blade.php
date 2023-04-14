f70367165233673cc0fdf12013b18e4dc034a5a2
vivi <vivisangadji06@gmail.com>
2023-04-15 07:29:13 +0900

    mengubah tampilan halaman home

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -3,7 +3,7 @@
 
     <head>
         <meta charset="utf-8" />
-        <title>{{ config('app.name') }} | Home</title>
+        <title>Home - {{ config('app.name') }}</title>
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <!-- favicon -->
         <link rel="shortcut icon" href="{{asset('assets/images/favicon.ico')}}">
@@ -90,7 +90,7 @@
                                 </div>
                             </div>
 
-                            <a href="{{ route('pesan.tiket') }}" target="_blank" class="btn btn-success mt-2 me-2"><i class="uil uil-ticket"></i> Buy Tickets</a>
+                            <a href="{{ route('form.pesan.tiket') }}" target="_blank" class="btn btn-success mt-2 me-2"><i class="uil uil-ticket"></i> Buy Tickets</a>
                         </div>
                     </div><!--end col-->
                 </div><!--end row-->
@@ -99,7 +99,7 @@
         <div class="position-relative">
             <div class="shape overflow-hidden text-footer">
                 <div class="text-center bg-white p-4">
-                    <h5 class="text-dark mb-0">Next Event : </h5>
+                    <h3 class="text-dark mb-0">Next Event : </h3>
                 </div>
             </div>
         </div>
@@ -111,12 +111,14 @@
                 <div class="card rounded shadow border-0 bg-light overflow-hidden">
                     <div class="row g-0 align-items-center">
                         <div class="col-lg-6">
-                            <img src="images/event/about.jpg" class="img-fluid" alt="">
+                            <img src="{{ asset('assets/images/event/b03.jpg') }}" class="img-fluid" alt="">
                         </div><!--end col-->
                         <div class="col-lg-6">
                             <div class="card-body section-title p-md-5">
-                                <h4 class="title mb-4">Business Startup Conference</h4>
-                                <p class="text-muted para-desc mb-0">Start working with Landrick that can provide everything you need to generate awareness, drive traffic, connect. Launch your campaign and benefit from our expertise on designing and managing conversion centered bootstrap v5 html page managing conversion centered bootstrap v5 html page.</p>
+                                <h4 class="title mb-4">{{ $latesEvent->nama_tiket }}</h4>
+                                <p class="text-muted para-desc mb-0">Harga Tiket: {{ rupiah($latesEvent->harga_tiket) }}</p>
+                                <p class="text-muted para-desc mb-0">Tanggal: {{ Carbon\Carbon::parse($latesEvent->tanggal_konser)->isoFormat('dddd, D MMMM Y') }}</p>
+                                <p class="text-muted para-desc mb-0">Pukul: {{ $latesEvent->jam }}</p>
                                 <div class="mt-4 pt-2">
                                     <!-- <a href="javascript:void(0)" class="btn btn-outline-primary">Read More <i class="uil uil-angle-right-b"></i></a> -->
                                 </div>
@@ -137,93 +139,6 @@
             </div>
         </div>
         <!--Shape End-->
-
-        <!-- News Start -->
-        <section class="section bg-light">
-            <div class="container">
-                <div class="row justify-content-center">
-                    <div class="col-12 text-center">
-                        <div class="section-title mb-4 pb-2">
-                            <h4 class="title mb-4">Event Blog</h4>
-                            <p class="text-muted para-desc mx-auto mb-0">Start working with <span class="text-primary fw-bold">Landrick</span> that can provide everything you need to generate awareness, drive traffic, connect.</p>
-                        </div>
-                    </div><!--end col-->
-                </div><!--end row-->
-
-                <div class="row">
-                    <div class="col-lg-4 col-md-6 mt-4 pt-2">
-                        <div class="card blog rounded border-0 shadow">
-                            <div class="position-relative">
-                                <img src="images/event/b01.jpg" class="card-img-top rounded-top" alt="...">
-                            <div class="overlay rounded-top bg-dark"></div>
-                            </div>
-                            <div class="card-body content">
-                                <h5><a href="javascript:void(0)" class="card-title title text-dark">Design your apps in your own way</a></h5>
-                                <div class="post-meta d-flex justify-content-between mt-3">
-                                    <ul class="list-unstyled mb-0">
-                                        <li class="list-inline-item me-2 mb-0"><a href="javascript:void(0)" class="text-muted like"><i class="uil uil-heart me-1"></i>33</a></li>
-                                        <li class="list-inline-item"><a href="javascript:void(0)" class="text-muted comments"><i class="uil uil-comment me-1"></i>08</a></li>
-                                    </ul>
-                                    <a href="page-blog-detail.html" class="text-muted readmore">Read More <i class="uil uil-angle-right-b align-middle"></i></a>
-                                </div>
-                            </div>
-                            <div class="author">
-                                <small class="text-light user d-block"><i class="uil uil-user"></i> Calvin Carlo</small>
-                                <small class="text-light date"><i class="uil uil-calendar-alt"></i> 13th August, 2019</small>
-                            </div>
-                        </div>
-                    </div><!--end col-->
-                    
-                    <div class="col-lg-4 col-md-6 mt-4 pt-2">
-                        <div class="card blog rounded border-0 shadow">
-                            <div class="position-relative">
-                                <img src="images/event/b02.jpg" class="card-img-top rounded-top" alt="...">
-                            <div class="overlay rounded-top bg-dark"></div>
-                            </div>
-                            <div class="card-body content">
-                                <h5><a href="javascript:void(0)" class="card-title title text-dark">How apps is changing the IT world</a></h5>
-                                <div class="post-meta d-flex justify-content-between mt-3">
-                                    <ul class="list-unstyled mb-0">
-                                        <li class="list-inline-item me-2 mb-0"><a href="javascript:void(0)" class="text-muted like"><i class="uil uil-heart me-1"></i>33</a></li>
-                                        <li class="list-inline-item"><a href="javascript:void(0)" class="text-muted comments"><i class="uil uil-comment me-1"></i>08</a></li>
-                                    </ul>
-                                    <a href="page-blog-detail.html" class="text-muted readmore">Read More <i class="uil uil-angle-right-b align-middle"></i></a>
-                                </div>
-                            </div>
-                            <div class="author">
-                                <small class="text-light user d-block"><i class="uil uil-user"></i> Calvin Carlo</small>
-                                <small class="text-light date"><i class="uil uil-calendar-alt"></i> 13th August, 2019</small>
-                            </div>
-                        </div>
-                    </div><!--end col-->
-                    
-                    <div class="col-lg-4 col-md-6 mt-4 pt-2">
-                        <div class="card blog rounded border-0 shadow">
-                            <div class="position-relative">
-                                <img src="images/event/b03.jpg" class="card-img-top rounded-top" alt="...">
-                            <div class="overlay rounded-top bg-dark"></div>
-                            </div>
-                            <div class="card-body content">
-                                <h5><a href="javascript:void(0)" class="card-title title text-dark">Smartest Applications for Business</a></h5>
-                                <div class="post-meta d-flex justify-content-between mt-3">
-                                    <ul class="list-unstyled mb-0">
-                                        <li class="list-inline-item me-2 mb-0"><a href="javascript:void(0)" class="text-muted like"><i class="uil uil-heart me-1"></i>33</a></li>
-                                        <li class="list-inline-item"><a href="javascript:void(0)" class="text-muted comments"><i class="uil uil-comment me-1"></i>08</a></li>
-                                    </ul>
-                                    <a href="page-blog-detail.html" class="text-muted readmore">Read More <i class="uil uil-angle-right-b align-middle"></i></a>
-                                </div>
-                            </div>
-                            <div class="author">
-                                <small class="text-light user d-block"><i class="uil uil-user"></i> Calvin Carlo</small>
-                                <small class="text-light date"><i class="uil uil-calendar-alt"></i> 13th August, 2019</small>
-                            </div>
-                        </div>
-                    </div><!--end col-->
-                </div><!--end row-->
-            </div><!--end container-->
-
-        </section><!--end section-->
-        <!-- End News -->
 
         <!-- Shape Start -->
         <div class="position-relative">
--- resources/views/layouts/admin/app.blade.php
+++ resources/views/layouts/admin/app.blade.php
@@ -7,7 +7,7 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <!-- Tell the browser to be responsive to screen width -->
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title>@yield('title') | {{ config('app.name') }}</title>
+    <title>@yield('title') | Admin {{ config('app.name') }}</title>
     <link rel="canonical" href="https://www.wrappixel.com/templates/ample-admin-lite/" />
     <!-- Favicon icon -->
     <link rel="icon" type="image/png" sizes="16x16" href="plugins/images/favicon.png">
